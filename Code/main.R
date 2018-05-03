ecb715c8ffd831b0e85b7760fab54b4babedffad
wilsonka <wilsonkl@uw.edu>
2018-05-03 15:23:07 -0700

    Update package versions

--- Code/main.R
+++ Code/main.R
@@ -7,21 +7,21 @@
 ##------------ Load Packages ------------##
 ###########################################
 
-library(INLA) # Version 0.0-1463562937
+library(INLA) # Version 17.12.01
 
-library(sp) # Version 1.2-4
-library(rgdal) # Version 1.2-5
-library(maptools) # Version 0.9-1
+library(sp) # Version 1.2-5
+library(rgdal) # Version 1.2-13
+library(maptools) # Version 0.9-2
 library(ggplot2) # Version 2.2.1
-library(rgeos) # Version 0.3-22
+library(rgeos) # Version 0.3-25
 library(plyr) # Version 1.8.4
-library(dplyr) # Version 0.5.0
+library(dplyr) # Version 0.7.4
 library(raster) # Version 2.5-8
 
-library(gridExtra) # Version 2.2.1
-library(cowplot) # Version 0.7.0
+library(gridExtra) # Version 2.3
+library(cowplot) # Version 0.9.2
 
-library(TMB) # Version 1.7.8
+library(TMB) # Version 1.7.11
 library(coda) # Version 0.19-1
 
 ###########################################
